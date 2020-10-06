285104f857e0017b7caef0e3f4bdb7cea490d4e6
hackteam <r.m.a.e.a.derks@gmail.com>
2020-10-06 14:23:26 +0200

    Updates

---
+++ _next/static/aj5e7PZ5DfCPK63UNPdye/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = (function(a){return {"/about":[a],"/blog/[slug]":[a]}}("static\u002Fchunks\u002F4fa6288b56176cadb82eb2bbe8478d69b4fdf9f0.76e40c5d308aa19ee010.js"));self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
