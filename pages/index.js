c578e41ead3ee4f235ce54fa521d12244fc7a6c9
hackteam <r.m.a.e.a.derks@gmail.com>
2020-10-07 10:30:07 +0200

    Add social images

--- pages/index.js
+++ pages/index.js
@@ -26,6 +26,7 @@ export default function Home({ posts }) {
           name="Description"
           content="Vandebron Engineering & Data. Leading the renewable energy transition with innovative solutions."
         />
+        <meta property="og:image" content={`https://www.vandebron.tech/images/hero.jpg`} key="ogimage" />
       </Head>
 
       <Container style={{ marginBottom: 60 }}>
