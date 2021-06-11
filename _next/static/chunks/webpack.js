45d704adb8428fe13e93f9837ef4afaa37f3416a
royderks <r.m.a.e.a.derks@gmail.com>
2021-06-11 10:31:19 +0200

    Updates

--- _next/static/chunks/webpack.js
+++ _next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "07933793437ab6ca5f16";
+/******/ 	var hotCurrentHash = "b5023429a2afa9e5d674";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
