a043179a4333cbf95df4963240b9a27187098d2e
royderks <r.m.a.e.a.derks@gmail.com>
2021-06-11 16:52:33 +0200

    Updates

--- _next/static/chunks/webpack.js
+++ _next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "881a300407233b291a3f";
+/******/ 	var hotCurrentHash = "b5a7ed160ec99e7fb567";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
---
+++ _next/static/webpack/pages/blog/[slug].79d82382aa14c5c0f870.hot-update.js
@@ -0,0 +1,14 @@
+webpackHotUpdate_N_E("pages/blog/[slug]",{
+
+/***/ "./public/posts/looking-back-at-vandebron-greentech-hackathon-2021.md":
+/*!****************************************************************************!*\
+  !*** ./public/posts/looking-back-at-vandebron-greentech-hackathon-2021.md ***!
+  \****************************************************************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+eval("throw new Error(\"Module parse failed: Assigning to rvalue (1:2)\\nYou may need an appropriate loader to handle this file type, currently no loaders are configured to process this file. See https://webpack.js.org/concepts#loaders\\n> ---\\n| title: Looking back at the Vandebron GreenTech Hackathon 2021 \\n| description: At the end of March, we organized a public hackathon to create solutions to tackle climate challenges. After having done internal hackathons, we thought it was time to share our technologies with other innovative people and companies. \");//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6IiIsImZpbGUiOiIuL3B1YmxpYy9wb3N0cy9sb29raW5nLWJhY2stYXQtdmFuZGVicm9uLWdyZWVudGVjaC1oYWNrYXRob24tMjAyMS5tZC5qcyIsInNvdXJjZVJvb3QiOiIifQ==\n//# sourceURL=webpack-internal:///./public/posts/looking-back-at-vandebron-greentech-hackathon-2021.md\n");
+
+/***/ })
+
+})
