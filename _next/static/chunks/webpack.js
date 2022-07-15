378d7b3129716e4a1ab0b07d4ccfc77a07529127
pietercusters <pietercusters@vandebron.nl>
2022-07-15 11:00:24 +0200

    Updates

--- _next/static/chunks/webpack.js
+++ _next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "b586f9709d7df729b368";
+/******/ 	var hotCurrentHash = "0bf277c82a1929cf161f";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
