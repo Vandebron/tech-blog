40e644a54ea21997c7b22e8e0479234c62e51856
Tim van Druenen <tim@Vandebrons-MacBook-Pro.local>
2023-01-11 13:15:42 +0000

    Updates

--- _next/static/chunks/webpack.js
+++ _next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "0bf277c82a1929cf161f";
+/******/ 	var hotCurrentHash = "f067a3e808c48e200abd";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
