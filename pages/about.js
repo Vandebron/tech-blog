34e3355bfe2308080c706df13441d90bf42dd4c6
Arno van den Berg <arnovandenberg@vandebron.nl>
2023-08-03 18:45:26 +0000

    updated about page

--- pages/about.js
+++ pages/about.js
@@ -5,9 +5,17 @@ import Markdown from "../components/Markdown";
 const ABOUT = `
 ![About Vandebron](images/technology-white-2.png)
 
-#### Vandebron is a greentech innovator leading the renewable energy transition with innovative solutions through technology, engineering and data.
+### Our mission is clear
 
-With more than 250 local sustainable energy sources spread throughout the Netherlands and with more than 200,000 connected households, we are well on the way to achieve our mission. At Vandebron we do things different. It is not just about dreams: we really want to bring change. We want to bring change by making the world more sustainable and we are all working hard to achieve that. We believe that in order to be able to provide good, green and sustainable energy, we need an energetic team of 150 colleagues with a clear opinion and positive vibes. We say it the way it is and we trust each other. With our pro-activity mindset we get to work and tackle problems. And we also enjoy working hard together very much. We ensure an ambitious working atmosphere, a delicious organic lunch, fresh coffee, lots of freedom and responsibility, parties on our roof terrace and a great enthusiastic team to work together with to achieve our mission!
+#### 100% renewable energy as soon as possible  
+
+To provide truly sustainable, renewable energy, produced locally in the Netherlands and void from dubious compensation systems from shady origins. With this roaring mission Vandebron introduced itself to the energy market in 2014. A refreshing message which was welcomed with open arms. Today, over 200.000 households have chosen Vandebron as their energy supplier.
+
+Since then, the energy market has changed. Simply producing and selling renewable energy proves to be no longer sufficient. With the growing share of sustainable energy on the grid, it is becoming increasingly important that customers can have access to sustainable energy 100% of the time. Even when the sun is not shining, or the wind is not blowing.
+
+In this evolving energy market, the role of Vandebron shifts as well. No longer are we the rebel of the market; more and more are we becoming the guide. We are the first Dutch energy supplier evolving into an energy tech company, who will lead in smart innovations the energy transition so desperately needs.
+
+100% renewable energy 100% of the time. That is the future.
 `;
 
 export default function About() {
