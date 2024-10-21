48b9c786f36a1859b3aae8be41b8f2902d5a4b72
jgf5013 <jgf5013@users.noreply.github.com>
2024-10-21 15:28:11 +0000

    Deploy preview for PR 47 🛫

---
+++ pr-preview/pr-47/greentech-hackathon/config/config.js
@@ -0,0 +1,56 @@
+export const FAQ = [
+  {
+    title: "Can I join without technical knowledge?",
+    description:
+      "Yes! We invite everyone to participate in the event, even without technical knowledge you can still create cool prototypes or use your imagination to make the perfect hackathon project come to life. Also, you can be teamed up with more technical people during our team matchmaking event.",
+  },
+  {
+    title: "How many team members do I need?",
+    description:
+      "Nobody likes to ride solo, so we advise you to sign up with a team of 2 to 5 people. If possible, it's best if you're able to combine the skill set of your team!",
+  },
+  {
+    title: "What if I don't have a team yet?",
+    description:
+      "No problem! Before the event, we'll organize a team matchmaking event for those who have signed up individually. Based on your skillset and the challenge you prefer working on we try to create an optimal team.",
+  },
+  {
+    title: "Can I only join the event with speakers?",
+    description:
+      "Yes, you can! If you don't have the time or not looking forward to joining the hackathon, you can still signup for the LinkedIn event and join us on April 1st to learn more about climate challenges and the energy transition.",
+  },
+];
+
+export const REGISTER_LINK_HACKATHON = "https://hopin.com/events/greentech-hackathon";
+export const REGISTER_LINK_EVENT =
+  "https://hopin.com/events/greentech-hackathon";
+
+export const SCHEDULE = [
+  {
+    description: "Opening & hacking 🚀",
+    activities: [
+      { time: "09:30 - 10:00", description: "Opening ceremony" },
+      { time: "10:00 - 11:30", description: "Team formation (optional)" },
+      { time: "16:30 - 17:30", description: "Day closing 🍻" },
+    ],
+  },
+  {
+    description: "Full day of hacking 🚀",
+    activities: [
+      { time: "09:30 - 10:00", description: "Day start ☕ (optional)" },
+      { time: "12:00 - 12:30", description: "Lunch & energizer" },
+      { time: "16:30 - 18:00", description: "Inspiration sessions 🎤" },
+    ],
+  },
+  {
+    description: "Finishing up & presentations 🏆",
+    activities: [
+      { time: "09:30 - 10:00", description: "Day start ☕" },
+      { time: "12:00 - 12:30", description: "Lunch & energizer" },
+      {
+        time: "16:30 - 17:30",
+        description: "Presentations, prizes & closing 🍻",
+      },
+    ],
+  },
+];
