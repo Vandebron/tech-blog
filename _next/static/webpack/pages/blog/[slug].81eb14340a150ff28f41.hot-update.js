615f243f8ca2ffefd4de54f3a574be2b75a78abc
John Fisher <jgf5013@gmail.com>
2025-02-21 15:20:46 +0100

    Updates

---
+++ _next/static/webpack/pages/blog/[slug].81eb14340a150ff28f41.hot-update.js
@@ -0,0 +1,14 @@
+webpackHotUpdate_N_E("pages/blog/[slug]",{
+
+/***/ "./public/posts/replacing-app-center.md":
+/*!**********************************************!*\
+  !*** ./public/posts/replacing-app-center.md ***!
+  \**********************************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+eval("throw new Error(\"Module parse failed: Assigning to rvalue (1:2)\\nYou may need an appropriate loader to handle this file type, currently no loaders are configured to process this file. See https://webpack.js.org/concepts#loaders\\n> ---\\n| title: Replacing App Center with GitHub Actions\\n| description: App Center is closing up shop on March 31st, 2025. So what's the plan for building mobile apps at Vandebron?\");//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6IiIsImZpbGUiOiIuL3B1YmxpYy9wb3N0cy9yZXBsYWNpbmctYXBwLWNlbnRlci5tZC5qcyIsInNvdXJjZVJvb3QiOiIifQ==\n//# sourceURL=webpack-internal:///./public/posts/replacing-app-center.md\n");
+
+/***/ })
+
+})
